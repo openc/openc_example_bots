bfb1f32789dd46b4d7e9a2eb4af66d32086b8467
Xavier Riley <xavriley@hotmail.com>
2014-06-04 15:40:22 +0100

    Renaming and clarifying medium bot example

--- medium_bot/medium_scraper.rb
+++ medium_bot/medium_scraper.rb
@@ -1,14 +1,19 @@
+# Intro
+# -----
+#
+# This bot scrapes procurement data for Uganda from [http://ppda.go.ug/tenderportal/](http://ppda.go.ug/tenderportal/)
+#
 # Prerequisites
 # -------------
 #
 # Load any gems you need first. Here we'll use `chronic` for date parsing,
 # `httpclient` and `nokogiri` to fetch and query the html pages and some 
-# helper methods from the `openc_bot` gem maintained by OpenCorporates
+# helper methods from the `openc_bot` gem maintained by OpenCorporates.
 require 'active_support/core_ext'
 require 'chronic'
 require 'nokogiri'
 require 'httpclient'
-# There are a number of helper methods covering text, csv, dates and more
+# There are a number of helper methods covering text, csv, dates and more.
 require 'openc_bot/helpers/text'
 
 BASE_URL = 'http://ppda.go.ug/tenderportal/'
@@ -18,7 +23,7 @@ BASE_URL = 'http://ppda.go.ug/tenderportal/'
 #
 # In this method, we loop through pages of search results breaking the loop
 # when we hit "No matches were found". We're passing these results pages onto
-# the `parse_results_page` method
+# the `parse_results_page` method.
 def get_all_notices
   start_notice = 0
   loop do
@@ -27,7 +32,7 @@ def get_all_notices
     break if page[/No matches were found/]
     results = parse_results_page(page)
     results.each do |result_hash|
-      # We then print the resultant hashes out as JSON
+      # We then print the resultant hashes out as JSON.
       puts JSON.dump(result_hash)
     end
     start_notice += 15
@@ -42,14 +47,15 @@ def parse_results_page(page)
   # In this case the data we want is in an HTML table
   table_rows = doc.search('table#tenderlisttop tr').to_a.select { |tr| tr.at('td') }
   # We take the cells from the first row as headers, normalising them to `snake_case`. 
-  # These will be used as hash keys later on
+  # These will be used as hash keys later on.
   headings = table_rows.shift.search('td').collect{ |td| td.inner_text.downcase.gsub(/[^\w\s]/,'').strip.gsub(/\s/,'_') }
-
-  # We then work through the rest of the rows, building a `key => value` hash based on the column headers
+  # We then work through the rest of the rows, building a `key => value` hash based on the column headers.
   results = table_rows.collect do |tr|
     res = tr.search('td').to_a.each.with_index.inject({}) do |hsh, (td, i)|
+      # Here we are using helpers from the [OpenCorporates `openc_bot` gem](https://github.com/openc/openc_bot).
       hsh[headings[i]] = OpencBot::Helpers::Text.strip_all_spaces(td.inner_text)
-      # We add an extra value to record the source url for this piece of data
+      # The data also contains links to a full text version of the tender notice.
+      # Here we record that url for future bots to process at a later date.
       hsh["#{headings[i]}_url"] = (BASE_URL + td.at('a')[:href]) if td.at('a')
       hsh
     end
@@ -66,5 +72,5 @@ def main
   get_all_notices
 end
 
-# Here's where the magic happens
+# Here's where the magic happens.
 main
